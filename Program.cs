b31f357419c284c00f9eefcd796a2fa997e7d2cc
Vedant <vedantnp200@gmail.com>
2023-05-22 18:26:45 +0530

    Snake and Ladder project created.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_and_Ladder_Simulator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---Snake and Ladder Simulator---");
+        }
+    }
+}
