77b26224cf736af4dd081c9f62a782327022ce04
Vedant <vedantnp200@gmail.com>
2023-05-30 16:15:50 +0530

    Snake and game played single player at position 0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Snake_and_Ladder_Simulator
 {
@@ -6,7 +7,15 @@ namespace Snake_and_Ladder_Simulator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("---Snake and Ladder Simulator---");
+            Console.WriteLine("---Snake and Ladder Simulator---\n");
+
+            // UC 1 - Snake and game played single player at position 0
+
+            int currentPosition = 0;
+            int finalPosition = 100;
+
+            Console.WriteLine("Start Position => " + currentPosition);
+
         }
     }
 }
