c8b136d60c66f6a1904fe08f4ca335f5dd1af2ec
Vedant <vedantnp200@gmail.com>
2023-05-30 23:18:13 +0530

    Play the game with two players

--- Program.cs
+++ Program.cs
@@ -13,19 +13,21 @@ namespace Snake_and_Ladder_Simulator
 
             int currentPosition = 0;
             int finalPosition = 100;
-            int diceRoll = 0;
-
             Console.WriteLine("Start Position => " + currentPosition);
+
+            int[] playerPosition = new int[2];
+            int currentPlayer = 0;
+            int diceRoll = 0;
 
             // UC 2 - The Player rolls the die to get a number between 1 to 6.
 
             Random random = new Random();
 
-            while (currentPosition < finalPosition)
+            while (playerPosition[currentPlayer] < finalPosition)
             {
                 int dieRoll = random.Next(1, 7);
 
-                Console.WriteLine("Player roll a die and get => " + dieRoll);
+                Console.WriteLine("Player " + (currentPlayer + 1) + " roll a die and get => " + dieRoll);
 
                 // UC 3 - The Player then checks for a Option. They are No Play, Ladder or Snake.
 
@@ -34,21 +36,38 @@ namespace Snake_and_Ladder_Simulator
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("No Play. \nThe Player Stays in Same Position");
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("**No Play**");
+                        Console.ResetColor();
+
+                        Console.WriteLine(" The Player Stays in Same Position");
                         break;
                     case 2:
-                        Console.WriteLine("Ladder. \nThe Player Moves Ahead By " + dieRoll + " Positions");
-                        currentPosition += dieRoll;
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.Write("**Ladder**");
+                        Console.ResetColor();
+
+                        Console.WriteLine(" Player" + (currentPlayer + 1) + " Moves Ahead By " + dieRoll + " Positions");
+                        
+                        if (playerPosition[currentPlayer] + dieRoll <= finalPosition)
+                            playerPosition[currentPlayer] += dieRoll;
+
                         break;
                     case 3:
-                        Console.WriteLine("Snake. \nThe Player Moves Behind By " + dieRoll + " Positions");
-                        currentPosition -= dieRoll;
-                        break;
-                }
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write("**Snake**");
+                        Console.ResetColor();
 
-                if (currentPosition < 0)
-                {
-                    currentPosition = 0;
+                        Console.WriteLine(" Player" + (currentPlayer + 1) + " Moves Behind By " + dieRoll + " Positions");
+
+                        playerPosition[currentPlayer] -= dieRoll;
+
+                        if (playerPosition[currentPlayer] < 0)
+                        {
+                            playerPosition[currentPlayer] = 0;
+                        }
+
+                        break;
                 }
                 
                 if (currentPosition > finalPosition)
@@ -56,12 +75,25 @@ namespace Snake_and_Ladder_Simulator
                     currentPosition = finalPosition;
                 }
                 diceRoll++;
-                Console.WriteLine("Current Position = " + currentPosition);
+                Console.WriteLine("Player " + (currentPlayer + 1) + " current position => " + playerPosition[currentPlayer]);
 
+                if (option != 1)
+                {
+                    currentPlayer = (currentPlayer + 1) % 2;
+                }
             }
 
-            Console.WriteLine("Congratulations! Player Has reached to the winning position");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nCongratulations! Player " + (currentPlayer + 1) + " Has reached to the winning position.");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Number of DiceRolls = " + diceRoll);
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Player " + (currentPlayer + 1) + " wins the game.");
+            Console.ResetColor();
         }
     }
 }
